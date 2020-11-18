0856305d69ad8374532764c52331c28fcfccae11
jens.richter@dna-evolutions.com <jens.richter@dna-evolutions.com>
2020-11-18 09:16:30 +0100

    Temporarily setting to azure endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  host: window["env"]["host"] || "http://localhost",
+  //host: window["env"]["host"] || "http://localhost",
+  host: window["env"]["host"] || "http://jopttouroptimizerlatest.westeurope.azurecontainer.io",
   port: window["env"]["port"] || "8081",
   apiProgress: '/api/optimization/stream/progress',
   apiWarning: '/api/optimization/stream/warning',
